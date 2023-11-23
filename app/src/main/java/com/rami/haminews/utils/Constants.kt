809c3dfe6eac31f0c538831889ba0e74f44cc312
Khamadekh Rami <khamadekh@gmail.com>
2023-11-23 17:48:43 +0300

    added module and providers in package di

---
+++ app/src/main/java/com/rami/haminews/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.rami.haminews.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "caefd24f6697410da61059d439dd8dff"
+    }
+}
